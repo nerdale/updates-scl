ba77d55a2747ed27f45298f2299b9111c3bdbe8b
carlacruzdiaz <carla@laboratoria.la>
2020-07-19 10:35:34 -0400

    nuevos cambios en Coders Fee

--- js/index.js
+++ js/index.js
@@ -2,14 +2,47 @@ const canv = document.getElementById('myChart').getContext('2d');
 let myChart = new Chart(canv, {
     type: 'bar',
     data: {
-        labels: ['SCL011', 'SCL012', 'SCL013'],
+        labels: ['SCL011', 'SCL012', 'SCL013', "SCL014"],
         datasets: [{
-            label: 'Estudiantes Activas',
-            data: [12, 29, 3],
+            label: 'Pagos Realizados',
+            data: [12, 29, 3, -10],
             backgroundColor: [
-                'rgba(253, 229, 77)',
-                'rgba(236, 69, 158)',
-                'rgba(113, 246, 154)'
+                '#fee54d',
+                '#EC459E',
+                '#71F69A',
+                "#5B5756"
+            ]
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        },
+        legend: {
+            display: false
+        },
+        responsive: true,
+        maintainAspectRatio: false
+    }
+});
+
+const canv2 = document.getElementById('myChart2').getContext('2d');
+let myChart2 = new Chart(canv2, {
+    type: 'bar',
+    data: {
+        labels: ['SCL011', 'SCL012', 'SCL013', "SCL014"],
+        datasets: [{
+            label: 'Pagos Realizados',
+            data: [12, 29, 3, -10],
+            backgroundColor: [
+                '#fee54d',
+                '#EC459E',
+                '#71F69A',
+                "#5B5756"
             ]
         }]
     },
